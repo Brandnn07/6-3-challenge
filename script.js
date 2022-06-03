10d1ad70638e32eb7448e97a0907d320549906c5
brandnn07 <brandcarter4@gmail.com>
2022-06-03 13:35:13 -0400

    done

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 // Create a function that takes two dates and returns the number of days between the first and second date.
-// getDays(
-//     new Date('June 14, 2019'),
-//     new Date('June 20, 2019')
-// )
-// var date1 = 'June 14, 2019';
-// var date2 = 'June 20, 2019';
-
-// function daysBetween(day1, day2) {
-//     var first = new Date(day1)
-//     let adate = first.getMonth();
-//     console.log(adate);
-// }
-
-// daysBetween()
 
 var date1 = 'December 29, 2018';
 var date2 = 'January 1, 2019';
